58bf6108ea02308dc67fcac68fd6b556d4a3fdb1
vickoman <vicmandlagasca@gmail.com>
2022-02-25 14:34:40 -0500

    Remove resolver and get from their own file

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 const { ApolloServer, gql} = require('apollo-server');
 const typeDefs = require('./db/schema');
-// Data
-const cursos = [
-    {
-        titulo: 'Curso de GraphQL',
-        tecnologia: 'GraphQL',
-        profesor: 'Victor Diaz'
-    },
-    {
-        titulo: 'Curso de NodeJS',
-        tecnologia: 'NodeJS',
-        profesor: 'Victor Diaz'
-    },
-    {
-        titulo: 'Curso de React',
-        tecnologia: 'React',
-        profesor: 'Victor Diaz'
-    }
-];
-
-
-
-// Resolvers
-const resolvers = {
-    Query: {
-        allCourses: () => cursos,
-        firstCourse: () => cursos[0]
-    }
-};
+const resolvers = require('./db/resolvers');
 
 // Inicializar servidor
 const server = new ApolloServer({
