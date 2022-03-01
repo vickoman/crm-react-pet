21fc2ec7b958df49c6fc40621380cf0c9d874fc6
vickoman <vicmandlagasca@gmail.com>
2022-02-28 21:42:08 -0500

    Add to connection in the entrypoint

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { ApolloServer, gql} = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
+const dbConnection = require('./config/db');
+
+// Connect with Database
+dbConnection();
 
 // Inicializar servidor
 const server = new ApolloServer({
