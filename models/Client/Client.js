72d87e93c5ffeb45a907252406095f388131f6f3
vickoman <vicmandlagasca@gmail.com>
2022-03-01 19:09:42 -0500

    Create Client model

---
+++ models/Client/Client.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const ClientSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    company: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    telephone: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Client', ClientSchema);
